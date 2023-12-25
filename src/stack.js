947d7606e481042efd44dd2f3ea299783187e96f
wingedseraph <r89647@gmail.com>
2023-12-25 13:55:39 +0300

    feat: done stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.item = [];
+  }
+  push(element) {
+    this.item.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.item.length === 0) {
+      return undefined;
+    }
+
+    return this.item.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.item.length === 0) {
+      return undefined;
+    }
+    return this.item[this.item.length - 1];
   }
 }
+const debug = new Stack();
+debug.push(0);
+// stack.push("word");
+console.debug(debug.peek());
 
 module.exports = {
-  Stack
+  Stack,
 };
